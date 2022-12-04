50235ba74875042001b2d44f28698e0447ac59ca
Pedro Jorquera <pjorquera@okode.com>
2022-12-04 19:10:12 +0100

    Added memory endpoint

--- src/main/java/com/okode/backend/controller/APIController.java
+++ src/main/java/com/okode/backend/controller/APIController.java
@@ -9,11 +9,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
 public class APIController {
 
-    @GetMapping("/version")
-    public Object getVersion() {
-        return new Object() {
-            public final String version = "development";
-        };
+    @GetMapping("/memory")
+    void memory() throws Exception {
+        final var size = 1024 * 1024 * 100;
+        var src = new byte[size];
+        var dst = new byte[size];
+        for(int i=0; i < size; ++i) {
+            src[i] = (byte) (i % 255);
+        }
+        System.arraycopy(src, 0, dst, 0, size);
+        for(int i=0; i < size; ++i) {
+            if (src[i] != dst[i]) throw new Exception("Error while comparing memory");
+        }
+    }
+
+    @GetMapping("/cpu")
+    void cpu() {
+
     }
 
 }
